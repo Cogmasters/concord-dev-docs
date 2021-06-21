b2d1d10a74be5ee283ad4adea12d6031d2f29bd8
LucasMull <muller.lucas@Hotmail.com>
2021-06-21 19:17:57 -0300

    fix: ..

--- conf.py
+++ conf.py
@@ -4,7 +4,7 @@ import subprocess, os
 read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
 if read_the_docs_build:
-    subprocess.call('cd ../doxygen; doxygen', shell=True)
+    subprocess.call('doxygen Doxyfile', shell=True)
 
 # Configuration file for the Sphinx documentation builder.
 #
